319e8fa9a15b28c3c2e879bfc1e3e67b894da798
Jaehwany <eejh22@gmail.com>
2022-10-24 11:07:51 +0900

    S07P31D208-46 Swagger 설정(test 전)

---
+++ backend/ddokbun/src/main/java/com/harryporter/ddokbun/config/SwaggerConfig.java
@@ -0,0 +1,35 @@
+package com.harryporter.ddokbun.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    // http://localhost:8080/swagger-ui/index.html
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(getApiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.harryporter.ddokbun.api"))
+                .paths(PathSelectors.ant("/**"))
+                .build();
+    }
+
+    private ApiInfo getApiInfo() {
+        return new ApiInfoBuilder()
+                .title("Ddokbun Swagger")
+                .description("[Ddokbun] REST API")
+                .version("1.0")
+                .build();
+    }
+}
