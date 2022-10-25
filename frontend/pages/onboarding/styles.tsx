71d9d50c499bde9be99d790b56473dab28dcba26
김채리 <kcr7812@naver.com>
2022-10-25 16:55:58 +0900

    S07P31D208-108 온보딩 소개 컴포넌트

--- frontend/pages/onboarding/styles.tsx
+++ frontend/pages/onboarding/styles.tsx
@@ -1,6 +1,8 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.div`
-  background-color: red;
-  font-family: "Black Han Sans", sans-serif;
+  margin: 0 auto;
+  max-width: 1240px;
+  width: 100%;
+  background-color: "#fafafa";
 `;
