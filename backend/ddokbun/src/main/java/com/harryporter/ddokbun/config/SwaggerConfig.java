4dda32b535cc0867ccc83754705bed78ae5cb49e
이동욱 <socialable@naver.com>
2022-10-25 10:31:18 +0900

    version 0.0

--- backend/ddokbun/src/main/java/com/harryporter/ddokbun/config/SwaggerConfig.java
+++ backend/ddokbun/src/main/java/com/harryporter/ddokbun/config/SwaggerConfig.java
@@ -12,7 +12,6 @@ import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
-@EnableSwagger2
 public class SwaggerConfig {
     // http://localhost:8080/swagger-ui/index.html
     @Bean
