7b45973310cd44eaf9afa27da447e42463bedbc1
신혜원 <hehe2you@gmail.com>
2022-10-27 15:35:53 +0900

    S07P31D208-99 캘린더 이미지 추가

--- frontend/common/Input/index.tsx
+++ frontend/common/Input/index.tsx
@@ -1,16 +1,36 @@
+import Image, { StaticImageData } from "next/image";
 import React from "react";
 import { BasicInput, SearchInputWrapper } from "./styles";
+import search from "../../assets/icon/search.png";
+import { useRouter } from "next/router";
 
 export const Input: React.FC<{
   type: string;
   placeholder: string;
   label: string;
-}> = ({ type, placeholder, label }) => {
+  saveInput: (value: string, identifier: string) => void;
+  identifier: string;
+  image: StaticImageData | null;
+}> = ({ type, placeholder, label, saveInput, identifier, image }) => {
+  const onInputChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    saveInput(event.target.value, identifier);
+  };
+
   return (
     <BasicInput>
       <label className="label-basic">
         {label}
-        <input className="input-basic" type={type} placeholder={placeholder} />
+        <input
+          onChange={onInputChangeHandler}
+          className="input-basic"
+          type={type}
+          placeholder={placeholder}
+        />
+        {image && (
+          <div className="icon">
+            <Image src={image} alt="calander-icon" />
+          </div>
+        )}
       </label>
     </BasicInput>
   );
@@ -19,8 +39,16 @@ export const Input: React.FC<{
 export const SearchInput: React.FC<{ placeholder: string }> = ({
   placeholder,
 }) => {
+  const router = useRouter();
+  const onShowSearchHandler = () => {
+    router.push("/search");
+  };
+
   return (
-    <SearchInputWrapper>
+    <SearchInputWrapper onClick={onShowSearchHandler}>
+      <div className="icon">
+        <Image src={search} alt="search-icon" />
+      </div>
       <input className="input-search" placeholder={placeholder} type="text" />
     </SearchInputWrapper>
   );
