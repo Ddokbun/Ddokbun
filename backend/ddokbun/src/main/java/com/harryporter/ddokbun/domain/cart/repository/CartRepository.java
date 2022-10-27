7561da620523f825fb233490a6e60f766ef4f4bb
손광진 <start_so@naver.com>
2022-10-28 00:31:38 +0900

    S07P31D208-109 N+1에 의한 코드 수정

--- backend/ddokbun/src/main/java/com/harryporter/ddokbun/domain/cart/repository/CartRepository.java
+++ backend/ddokbun/src/main/java/com/harryporter/ddokbun/domain/cart/repository/CartRepository.java
@@ -14,6 +14,6 @@ public interface CartRepository extends JpaRepository<Cart, CartId> {
 
     //장바구니를 조회하는데, 상품목록을 더해 response에 추가하기 위해 , 상품정보들도 가져온다.
     //유저를 특정할 시, 1:N관계에서 1:1의 결과 밖에 나오지않지만 일단 해두자.
-    @Query("SELECT DISTINCT c FROM Cart c LEFT JOIN FETCH c.item WHERE c.user.userSeq = :userSeq")
+    @Query("SELECT DISTINCT c FROM Cart c LEFT JOIN FETCH c.item i LEFT JOIN FETCH i.plant WHERE c.user.userSeq = :userSeq")
     List<Cart> findByUserSeqWithFetch(@Param("userSeq") Long userSeq);
 }
