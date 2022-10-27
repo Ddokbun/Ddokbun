4bb86066755969317ad07922ee624440753e0aa8
손광진 <start_so@naver.com>
2022-10-27 17:54:00 +0900

    S07P31D208-114 장바구니 아이템 삭제 완료 및 로그 추가

--- backend/ddokbun/src/main/java/com/harryporter/ddokbun/domain/cart/service/CartService.java
+++ backend/ddokbun/src/main/java/com/harryporter/ddokbun/domain/cart/service/CartService.java
@@ -2,4 +2,6 @@ package com.harryporter.ddokbun.domain.cart.service;
 
 public interface CartService {
     int enrollCartItem(Long itemSeq, Long userSeq);
+
+    int deleteCartItem(Long itemSeq, Long userSeq);
 }
