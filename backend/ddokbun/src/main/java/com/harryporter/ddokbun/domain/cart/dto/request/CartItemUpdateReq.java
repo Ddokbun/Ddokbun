ecbb3f8e1a70b62b1f9eeda390f489e1b6a204aa
손광진 <start_so@naver.com>
2022-10-27 22:10:48 +0900

    S07P31D208-115 장바구니 아이템 수량 조절 API 완료

---
+++ backend/ddokbun/src/main/java/com/harryporter/ddokbun/domain/cart/dto/request/CartItemUpdateReq.java
@@ -0,0 +1,10 @@
+package com.harryporter.ddokbun.domain.cart.dto.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CartItemUpdateReq {
+    private Integer quantity;
+}
--- backend/ddokbun/src/main/java/com/harryporter/ddokbun/domain/cart/service/CartService.java
+++ backend/ddokbun/src/main/java/com/harryporter/ddokbun/domain/cart/service/CartService.java
@@ -1,7 +1,11 @@
 package com.harryporter.ddokbun.domain.cart.service;
 
+import com.harryporter.ddokbun.domain.cart.dto.CartDto;
+
 public interface CartService {
     int enrollCartItem(Long itemSeq, Long userSeq);
 
     int deleteCartItem(Long itemSeq, Long userSeq);
+
+    CartDto updateCartItem(CartDto cartDto);
 }
