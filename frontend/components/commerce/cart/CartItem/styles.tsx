9246dbad05ca2643f988892bfde36e84dd19facc
heogeon0 <heo3793@naver.com>
2022-10-28 00:33:10 +0900

    S07P31D208-133 장바구니페이지 완료

--- frontend/components/commerce/cart/CartItem/styles.tsx
+++ frontend/components/commerce/cart/CartItem/styles.tsx
@@ -49,4 +49,57 @@ export const Wrapper = styled.div`
       color: ${props => props.theme.color.darkGreen};
     }
   }
+  @media screen and (${props => props.theme.mobile}) {
+    padding: 5px;
+    display: flex;
+    background-color: #fafafa;
+    display: grid;
+    grid-template-columns: 180px 1fr;
+    grid-template-rows: 100px auto;
+    grid-gap: 10px;
+    position: relative;
+    .grid-left {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      grid-area: 1 / 1/ 3/ 2;
+    }
+
+    .grid-center {
+      position: relative;
+      padding: 20px 10px;
+      h2 {
+        font-size: 30px;
+        font-family: ${props => props.theme.font.TitleFont};
+        color: ${props => props.theme.color.darkGreen};
+      }
+      .count {
+        position: absolute;
+        height: 20px;
+        left: 10px;
+
+        top: 110px;
+        font-size: 19px;
+        font-weight: 600;
+        display: flex;
+        gap: 8px;
+
+        .handler {
+          cursor: pointer;
+        }
+      }
+    }
+    .grid-right {
+      position: absolute;
+      grid-area: 2 / 2 / 3 / 3;
+      padding: 10px 20px;
+      bottom: 10px;
+      right: 10px;
+      h2 {
+        font-size: 25px;
+        font-family: ${props => props.theme.font.EnglishFont};
+        color: ${props => props.theme.color.darkGreen};
+      }
+    }
+  }
 `;
