0358acbbb2ca4210606e72f0ce8252ed68b6d975
신혜원 <hehe2you@gmail.com>
2022-10-27 10:46:34 +0900

    S07P31D208-73 디지털 트윈 카드 상단 아이콘 구현

---
+++ frontend/components/manage/DigitalTwin/Box.tsx
@@ -0,0 +1,27 @@
+import React, { useRef, useState } from "react";
+import * as THREE from "three";
+import { createRoot } from "react-dom/client";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { Box2 } from "three";
+
+const Box = (props: JSX.IntrinsicElements["mesh"]) => {
+  const mesh = useRef<THREE.Mesh>(null!);
+  const [hovered, setHover] = useState(false);
+  const [active, setActive] = useState(false);
+  useFrame((state, delta) => (mesh.current.rotation.x += 0.01));
+  return (
+    <mesh
+      {...props}
+      ref={mesh}
+      scale={active ? 1.5 : 1}
+      onClick={event => setActive(!active)}
+      onPointerOver={event => setHover(true)}
+      onPointerOut={event => setHover(false)}
+    >
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={hovered ? "hotpink" : "orange"} />
+    </mesh>
+  );
+};
+
+export default Box;
