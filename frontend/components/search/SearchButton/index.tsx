d83424811dabc759962c404718f52f5ddce4746f
kimchaeri <kcr7812@naver.com>
2022-10-27 14:10:44 +0900

    S07P31D208-123 검색 컴포넌트 구현 및 폴더구조 정리

---
+++ frontend/components/search/SearchButton/index.tsx
@@ -0,0 +1,16 @@
+import react from "react";
+import { Wrapper } from "./styles";
+
+const CommerceContents = () => {
+  return (
+    <Wrapper>
+      <div className="subcontent-wrap">
+        <div className="title">
+          <h2>버튼 들어갑니다</h2>
+        </div>
+      </div>
+    </Wrapper>
+  );
+};
+
+export default CommerceContents;
--- frontend/pages/onboarding/index.tsx
+++ frontend/pages/onboarding/index.tsx
@@ -1,11 +1,10 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-
-import MainContents from "../../components/Onboarding/MainContents";
-import SubContents from "../../components/Onboarding/SubContents";
-import CommerceContents from "../../components/Onboarding/CommerceContents";
-import SearchContents from "../../components/Onboarding/SearchContents";
-import RecommendContents from "../../components/Onboarding/RecommendContents";
+import MainContents from "../../components/onboarding/MainContents";
+import SubContents from "../../components/onboarding/SubContents";
+import CommerceContents from "../../components/onboarding/CommerceContents";
+import SearchContents from "../../components/onboarding/SearchContents";
+import RecommendContents from "../../components/onboarding/RecommendContents";
 
 const Onboarding: NextPage = () => {
   return (
