a6dba2ad089a3299adbf463595732da48bc52fee
손광진 <start_so@naver.com>
2022-10-30 17:17:29 +0900

    S07P31D208-149 설문 내용 조회 API 완료

--- backend/ddokbun/src/main/java/com/harryporter/ddokbun/domain/survey/service/SurveyService.java
+++ backend/ddokbun/src/main/java/com/harryporter/ddokbun/domain/survey/service/SurveyService.java
@@ -1,6 +1,11 @@
 package com.harryporter.ddokbun.domain.survey.service;
 
+import com.harryporter.ddokbun.domain.survey.dto.response.SurveyResponse;
+
+import java.util.List;
+
 public interface SurveyService {
 
 
+    List<SurveyResponse> getSurveys();
 }
