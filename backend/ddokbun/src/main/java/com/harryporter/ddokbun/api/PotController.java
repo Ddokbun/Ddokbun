dbefbfafde7032b5ef1d167a201d54f517b6eb28
Dongwook <socialable@naver.com>
2022-10-31 16:11:43 +0900

    S07P31D208-94-수동물주기API개발

--- backend/ddokbun/src/main/java/com/harryporter/ddokbun/api/PotController.java
+++ backend/ddokbun/src/main/java/com/harryporter/ddokbun/api/PotController.java
@@ -15,9 +15,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.parameters.P;
 import org.springframework.web.bind.annotation.*;
 import springfox.documentation.annotations.ApiIgnore;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @RequestMapping("/pot")
@@ -34,10 +36,9 @@ public class PotController {
     @ApiOperation(value = "화분 등록")
     @RequestMapping(method = RequestMethod.POST)
     public ResponseEntity<?> registerPot(@RequestBody RegisterPotRequest registerPotRequest,@ApiIgnore @AuthenticationPrincipal UserDto principal) {
-        // 나중에 실제 유저가 들어오면 삭제할 로직
-        UserDto userDto = ((UserDto)principal);
+        UserDto userDto = principal;
 
-        log.info("화분 등록 컨트롤러 진입 :: 화분시리얼 : {} :: 사용자 Seq : {}", registerPotRequest.getPotSerial(), userDto.getUserSeq());
+        log.info("화분 등록 컨트롤러 진입 :: 화분시리얼 : {} :: 사용자 Seq : {}", registerPotRequest.getPotSerial(), principal.getUserSeq());
         RegisterPotResponse registerPotResponse = potService.registerPot(registerPotRequest, userDto.getUserSeq());
 
         ResponseFrame<?> responseFrame = ResponseFrame.ofOKResponse("화분 등록에 성공했습니다.",registerPotResponse);
@@ -49,13 +50,12 @@ public class PotController {
     @ApiOperation(value = "화분 등록 해제")
     @RequestMapping(value = "/{potSeq}", method = RequestMethod.DELETE)
     public ResponseEntity<?> unregisterPot(@PathVariable("potSeq") String potSerial,@ApiIgnore @AuthenticationPrincipal UserDto principal) {
-        // 나중에 실제 유저가 들어오면 삭제할 로직
-        UserDto userDto = ((UserDto)principal);
+        UserDto userDto = principal;
 
         log.info("화분 등록 해제 컨트롤러 진입 :: 화분시리얼 : {} :: 사용자 Seq : {}", potSerial, userDto.getUserSeq());
         potService.unregisterPot(potSerial, userDto.getUserSeq());
 
-        ResponseFrame<?> responseFrame = ResponseFrame.ofOKResponse("화분 등록에 성공했습니다.", null);
+        ResponseFrame<?> responseFrame = ResponseFrame.ofOKResponse("화분 등록 해제에 성공했습니다.", null);
         responseFrame.setCode(202);
         log.info("화분 등록에 완료 :: response : {}", responseFrame.toString());
         return new ResponseEntity<>(responseFrame, HttpStatus.OK);
@@ -76,13 +76,26 @@ public class PotController {
     @ApiOperation(value = "검색한 식물의 정보 받기")
     @RequestMapping(value = "/{plantSeq}/plant-info", method = RequestMethod.GET)
     public ResponseEntity<?> potPlantInfo(@PathVariable("plantSeq") Long plantSeq) {
-        log.info("검색한 식물 정보 받기 컨트롤러 진입 :: 식물시리얼 : {}", plantSeq);
+        log.info("검색한 식물 정보 받기 컨트롤러 진입 :: 식물번호 : {}", plantSeq);
         SearchPlantInfoResponse searchPlantInfoResponse = plantService.searchPlantInfo(plantSeq);
         log.info("식물 정보 받기 성공 :: 식물정보 {}", searchPlantInfoResponse);
 
-        ResponseFrame<?> responseFrame = ResponseFrame.ofOKResponse("식물 정보를 받는데 성공했씁니다.", searchPlantInfoResponse);
+        ResponseFrame<?> responseFrame = ResponseFrame.ofOKResponse("식물 정보를 받는데 성공했습니다.", searchPlantInfoResponse);
         responseFrame.setCode(200);
 
         return new ResponseEntity<>(responseFrame, HttpStatus.OK);
     }
+
+    @ApiOperation(value = "수동 물주기")
+    @RequestMapping(value = "/{potSeq}/water", method = RequestMethod.POST)
+    public ResponseEntity<?> potWaterApply(@PathVariable("potSeq") String potSeq, @ApiIgnore @AuthenticationPrincipal UserDto principal) {
+        log.info("화분 수동 물주기 컨트롤러 진입 :: 화분시리얼 {}", potSeq);
+        potService.applyWater(potSeq, principal.getUserSeq());
+
+        log.info("화분 수동 물주기 완료 :: 물준 일자 : {}", LocalDate.now());
+        ResponseFrame<?> responseFrame = ResponseFrame.ofOKResponse("화분에 물을 주는데 성공했습니다.", null);
+
+        return new ResponseEntity<>(responseFrame, HttpStatus.OK);
+    }
+
 }
