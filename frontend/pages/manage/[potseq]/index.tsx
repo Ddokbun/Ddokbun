50c1c5bdbe43bf676e52c4bfea0bf422a1fd9150
신혜원 <hehe2you@gmail.com>
2022-10-31 17:38:55 +0900

    S07P31D208-88 manage progress bar 디자인

--- frontend/pages/manage/[potseq]/index.tsx
+++ frontend/pages/manage/[potseq]/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import SimpleGraph from "../../../common/Graph/SimpleGraph";
 import WeekPicker from "../../../components/manage/add/WeekPicker";
 import DigitalTwin from "../../../components/manage/DigitalTwin";
 import PlantStatus from "../../../components/manage/PlantStatus";
@@ -12,6 +13,14 @@ const PlantCare = () => {
   return (
     <Wrapper>
       <DigitalTwin />
+      <span className="title">모든 환경이 최상이예요!</span>
+      <SimpleGraph
+        pages="manage"
+        temper={"50%"}
+        water={"50%"}
+        humid={"50%"}
+        waterBottle={"50%"}
+      />
       <WeekPicker showDetailHandler={showDetailHandler} />
       <PlantStatus />
     </Wrapper>
