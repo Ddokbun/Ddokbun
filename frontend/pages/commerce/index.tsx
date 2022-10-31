b7c9dc2e594ce98a7ce467c0223a68f5ea07f4eb
kimchaeri <kcr7812@naver.com>
2022-10-31 17:17:56 +0900

    S07P31D208-180 설문조사 컴포넌트 레이아웃

--- frontend/pages/commerce/index.tsx
+++ frontend/pages/commerce/index.tsx
@@ -1,11 +1,13 @@
 import { NextPage } from "next";
 import { Wrapper } from "../../styles/commerce/styles";
 import MainProduct from "../../components/commerce/main/MainProduct";
+import SurveyBanner from "../../components/commerce/main/SurveyBanner";
 
 const Commerce: NextPage = () => {
   return (
     <Wrapper>
       <MainProduct />
+      <SurveyBanner />
       <MainProduct />
       <MainProduct />
       <MainProduct />
