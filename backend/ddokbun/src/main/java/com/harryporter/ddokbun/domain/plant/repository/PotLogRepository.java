3b835fbfe5d09a6e140bd7f934090615263b5202
이동욱 <socialable@naver.com>
2022-11-02 03:23:28 +0900

    S07P31D208-236-화분온도-로그값-조회-API개발

--- backend/ddokbun/src/main/java/com/harryporter/ddokbun/domain/plant/repository/PotLogRepository.java
+++ backend/ddokbun/src/main/java/com/harryporter/ddokbun/domain/plant/repository/PotLogRepository.java
@@ -1,6 +1,7 @@
 package com.harryporter.ddokbun.domain.plant.repository;
 
 import com.harryporter.ddokbun.domain.plant.dto.response.PotTemperatureLogResponse;
+import com.harryporter.ddokbun.domain.plant.entity.Pot;
 import com.harryporter.ddokbun.domain.plant.entity.PotLog;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
