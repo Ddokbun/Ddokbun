9e58f1a3b6d546b3778e1c029d593a6987b49c16
신혜원 <hehe2you@gmail.com>
2022-11-02 12:52:32 +0900

    S07P31D208-73 library에 지정되지 않는 type any 설정

--- frontend/components/Flower/index.tsx
+++ frontend/components/Flower/index.tsx
@@ -2,11 +2,13 @@ import React, { useRef } from "react";
 import { useGLTF } from "@react-three/drei";
 
 const Flower = () => {
-  const { nodes, materials } = useGLTF("/models/model.glb");
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const { nodes, materials }: any = useGLTF("/models/model.glb");
+
   return (
     <group dispose={null}>
       <group rotation={[-Math.PI / 2, 0, 0]}>
-        <group rotation={[Math.PI / 2, 0, 0]} scale={0.01}>
+        <group rotation={[Math.PI / 2, 0, 0]} scale={0.05}>
           <mesh
             geometry={nodes.pCylinder20_lambert1_0.geometry}
             material={materials.lambert1}
