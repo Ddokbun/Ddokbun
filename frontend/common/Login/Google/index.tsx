2c1038dd3ee5e55b77b9faa45bfc3f0d782b58c9
cherry <kcr7812@naver.com>
2022-11-04 00:03:37 +0900

    S07P31D208-105 구글 로그인 버튼 제작

--- frontend/common/Login/Google/index.tsx
+++ frontend/common/Login/Google/index.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
-import google from "../../../assets/googleLogin.jpg";
+import google from "../../../assets/googleLogin.png";
 
 const GoogleLogin = () => {
   const GOOGLE_AUTH_URL = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${process.env.NEXT_PUBLIC_CLIENT_ID_GOOGLE}&redirect_uri=${process.env.NEXT_PUBLIC_REDIRECT_URI_GOOGLE}&response_type=code&scope=${process.env.NEXT_PUBLIC_RESPONSE_TYPE}&access_type=${process.env.NEXT_PUBLIC_ACCESS_TYPE}`;
