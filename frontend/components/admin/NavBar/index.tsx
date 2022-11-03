f6cc25e1f370238783ed4fcd17343f6c8deec4d4
kimchaeri <kcr7812@naver.com>
2022-11-03 17:32:29 +0900

    S07P31D208-206 관리자 레이아웃 완료

--- frontend/components/admin/NavBar/index.tsx
+++ frontend/components/admin/NavBar/index.tsx
@@ -1,12 +1,29 @@
 import React from "react";
 import { Wrapper } from "./styles";
 import Link from "next/link";
-import { useRouter } from "next/router";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 
 const AdminNav = () => {
   return (
     <Wrapper>
-      <h1>네브바 들어갈 자리</h1>
+      <div className="admin-nav">
+        <div className="title">
+          <Link href={"/admin"}>Admin DashBoard</Link>
+        </div>
+        <div className="link-list">
+          <Link href={"/admin/commerce"}>Commerce</Link>
+        </div>
+        <div className="link-list">
+          <Link href={"/admin/user"}>User</Link>
+        </div>
+        <div className="link-list">
+          <Link href={"/admin/product"}>Product</Link>
+        </div>
+        <div className="link-list">
+          <Link href={"/admin/product/register"}>Product Register</Link>
+        </div>
+      </div>
     </Wrapper>
   );
 };
