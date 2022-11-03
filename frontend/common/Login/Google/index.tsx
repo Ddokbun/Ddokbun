bd6527eb3373b305c6ea3eced056f908ca82edcf
cherry <kcr7812@naver.com>
2022-11-03 23:41:14 +0900

    S07P31D208-105 로그인 common 변경

---
+++ frontend/common/Login/Google/index.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+import Link from "next/link";
+import google from "../../../assets/googleLogin.jpg";
+
+const GoogleLogin = () => {
+  const GOOGLE_AUTH_URL = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${process.env.NEXT_PUBLIC_CLIENT_ID_GOOGLE}&redirect_uri=${process.env.NEXT_PUBLIC_REDIRECT_URI_GOOGLE}&response_type=code&scope=${process.env.NEXT_PUBLIC_RESPONSE_TYPE}&access_type=${process.env.NEXT_PUBLIC_ACCESS_TYPE}`;
+
+  return (
+    <Link href={GOOGLE_AUTH_URL}>
+      <div className="google-button">
+        <Image src={google} alt="구글 로그인 버튼" width={350} height={50} />
+      </div>
+    </Link>
+  );
+};
+
+export default GoogleLogin;
--- frontend/components/welcome/MainItem/index.tsx
+++ frontend/components/welcome/MainItem/index.tsx
@@ -1,17 +1,11 @@
 import { Wrapper } from "./styles";
 import Image from "next/image";
-import Link from "next/link";
 import mainImg from "../../../assets/onboarding/mainImg.jpg";
-import kakao from "../../../assets/kakaoLogin.png";
-import google from "../../../assets/googleLogin.jpg";
 import { LoginButton } from "../../../common/Button";
+import KakaoLogin from "../../../common/Login/Kakao";
+import GoogleLogin from "../../../common/Login/Google";
 
 const MainItem = () => {
-  const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_CLIENT_ID_KAKAO}&redirect_uri=${process.env.NEXT_PUBLIC_REDIRECT_URI_KAKAO}&response_type=code`;
-
-  const GOOGLE_AUTH_URL = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${process.env.NEXT_PUBLIC_CLIENT_ID_GOOGLE}&redirect_uri=${process.env.NEXT_PUBLIC_REDIRECT_URI_GOOGLE}&response_type=code&scope=${process.env.NEXT_PUBLIC_RESPONSE_TYPE}&access_type=${process.env.NEXT_PUBLIC_ACCESS_TYPE}`;
-
-  console.log(GOOGLE_AUTH_URL);
   return (
     <Wrapper>
       <div className="banner-wrap">
@@ -28,29 +22,9 @@ const MainItem = () => {
           <h2>스마트 화분 어플리케이션</h2>
         </div>
         <div className="login-button">
-          <Link href={KAKAO_AUTH_URL}>
-            <div>
-              <Image
-                src={kakao}
-                alt="카카오 로그인 버튼"
-                width={350}
-                height={50}
-              />
-            </div>
-          </Link>
-          <Link href={GOOGLE_AUTH_URL}>
-            <div className="google-button">
-              <Image
-                src={google}
-                alt="구글 로그인 버튼"
-                width={350}
-                height={50}
-              />
-            </div>
-          </Link>
-          <div>
-            <LoginButton path="/commerce" />
-          </div>
+          <KakaoLogin></KakaoLogin>
+          <GoogleLogin></GoogleLogin>
+          <LoginButton path="/commerce" />
         </div>
       </div>
     </Wrapper>
