7e76dc6e0de63f061e27bed1a2ba0a01f14fc4b2
kimchaeri <kcr7812@naver.com>
2022-11-03 17:14:53 +0900

    S07P31D208-206 build 에러 수정

--- frontend/components/admin/NavBar/styles.tsx
+++ frontend/components/admin/NavBar/styles.tsx
@@ -1,26 +1,3 @@
-import tw from "tailwind-styled-components";
+import styled from "styled-components";
 
-export const Wrapper = tw.div`
-    nav {
-        md:left-0 
-        md:block 
-        md:fixed 
-        md:top-0 
-        md:bottom-0 
-        md:overflow-y-auto 
-        md:flex-row 
-        md:flex-nowrap 
-        md:overflow-hidden 
-        shadow-xl 
-        bg-white 
-        flex 
-        flex-wrap 
-        items-center 
-        justify-between 
-        relative 
-        md:w-64 
-        z-10 
-        py-4 
-        px-6
-    }
-`;
+export const Wrapper = styled.div``;
