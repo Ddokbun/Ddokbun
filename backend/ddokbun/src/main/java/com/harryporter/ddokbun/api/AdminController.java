31c3e736aa8e0358ac5e163c45cc1396faac3a3c
이재환 <eejh22@gmail.com>
2022-11-04 10:38:01 +0900

    S07P31D208-200 유사 식물 조회 API 구현 완료

--- backend/ddokbun/src/main/java/com/harryporter/ddokbun/api/AdminController.java
+++ backend/ddokbun/src/main/java/com/harryporter/ddokbun/api/AdminController.java
@@ -35,14 +35,14 @@ public class AdminController {
     private final ItemService itemService;
     private final OrderService orderService;
 
-    @ApiOperation(value = "식물 데이터 조회 (Test용 1번 데이터 조회)")
+    @ApiOperation(value = "식물 데이터 조회")
     @GetMapping ("/plant")
     public ResponseEntity<?> getPlant(@ApiIgnore @AuthenticationPrincipal UserDto userDto){
 //        if(!userDto.getUserRole().equals("ROLE_ADMIN"))
 //            throw new GeneralException(ErrorCode.BAD_REQUEST,"관리자 계정이 아닙니다");
 //        log.info("관리자 접속 완료");
 
-        ResponseFrame<?> res =  ResponseFrame.ofOKResponse("Success",plantService.getPlant());
+        ResponseFrame<?> res =  ResponseFrame.ofOKResponse("식물 데이터를 반환합니다.",plantService.getPlant());
         return new ResponseEntity<>(res, HttpStatus.OK);
     }
 
@@ -52,8 +52,8 @@ public class AdminController {
     public ResponseEntity<?> insertPlant(@RequestBody PlantDto plantDto,@ApiIgnore @AuthenticationPrincipal UserDto userDto){
         if(!userDto.getUserRole().equals("ROLE_ADMIN"))
             throw new GeneralException(ErrorCode.BAD_REQUEST,"관리자 계정이 아닙니다");
-        log.info("관리자 접속 완료");
-        ResponseFrame<?> res =  ResponseFrame.ofOKResponse("Success",plantService.insertPlant(plantDto));
+        log.info("관리자 :: 식물 데이터 등록 API");
+        ResponseFrame<?> res =  ResponseFrame.ofOKResponse("식물 데이터 등록에 성공했습니다",plantService.insertPlant(plantDto));
         return new ResponseEntity<>(res, HttpStatus.OK);
     }
 
@@ -62,8 +62,8 @@ public class AdminController {
     public ResponseEntity<?> updatePlant(@RequestBody PlantDto plantDto,@ApiIgnore @AuthenticationPrincipal UserDto userDto){
         if(!userDto.getUserRole().equals("ROLE_ADMIN"))
             throw new GeneralException(ErrorCode.BAD_REQUEST,"관리자 계정이 아닙니다");
-        log.info("관리자 접속 완료");
-        ResponseFrame<?> res =  ResponseFrame.ofOKResponse("Success",plantService.updatePlant(plantDto));
+        log.info("관리자 :: 식물 데이터 변경 API");
+        ResponseFrame<?> res =  ResponseFrame.ofOKResponse("식물 데이터 변경에 성공했습니다.",plantService.updatePlant(plantDto));
         return new ResponseEntity<>(res, HttpStatus.OK);
     }
 
@@ -72,7 +72,8 @@ public class AdminController {
     public ResponseEntity<?> insertProduct(@RequestBody InsertItemDto itemDto, @ApiIgnore @AuthenticationPrincipal UserDto userDto){
         if(!userDto.getUserRole().equals("ROLE_ADMIN"))
             throw new GeneralException(ErrorCode.BAD_REQUEST,"관리자 계정이 아닙니다");
-        ResponseFrame<?> res =  ResponseFrame.ofOKResponse("Success",itemService.insertItem(itemDto));
+        log.info("관리자 :: 상품 등록 API");
+        ResponseFrame<?> res =  ResponseFrame.ofOKResponse("상품이 등록되었습니다",itemService.insertItem(itemDto));
         return new ResponseEntity<>(res, HttpStatus.OK);
     }
 
@@ -81,7 +82,8 @@ public class AdminController {
     public ResponseEntity<?> updateProduct(@RequestBody UpdateItemDto itemDto, @ApiIgnore @AuthenticationPrincipal UserDto userDto){
         if(!userDto.getUserRole().equals("ROLE_ADMIN"))
             throw new GeneralException(ErrorCode.BAD_REQUEST,"관리자 계정이 아닙니다");
-        ResponseFrame<?> res =  ResponseFrame.ofOKResponse("Success",itemService.updateItem(itemDto));
+        log.info("관리자 :: 상품 변경 API");
+        ResponseFrame<?> res =  ResponseFrame.ofOKResponse("상품 정보 변경에 성공했습니다.",itemService.updateItem(itemDto));
         return new ResponseEntity<>(res, HttpStatus.OK);
     }
 
@@ -90,7 +92,8 @@ public class AdminController {
     public ResponseEntity<?> deleteProduct(@PathVariable long itemSeq, @ApiIgnore @AuthenticationPrincipal UserDto userDto){
         if(!userDto.getUserRole().equals("ROLE_ADMIN"))
             throw new GeneralException(ErrorCode.BAD_REQUEST,"관리자 계정이 아닙니다");
-        ResponseFrame<?> res =  ResponseFrame.ofOKResponse("Success",itemService.deleteItem(itemSeq));
+        log.info("관리자 :: 상품 삭제 API");
+        ResponseFrame<?> res =  ResponseFrame.ofOKResponse("상품 삭제에 성공했습니다.",itemService.deleteItem(itemSeq));
         return new ResponseEntity<>(res, HttpStatus.OK);
     }
 
@@ -99,7 +102,52 @@ public class AdminController {
     public ResponseEntity<?> updateOrderStatus(@RequestBody OrderStatusDto orderStatusDto,@ApiIgnore @AuthenticationPrincipal UserDto userDto){
         if(!userDto.getUserRole().equals("ROLE_ADMIN"))
             throw new GeneralException(ErrorCode.BAD_REQUEST,"관리자 계정이 아닙니다");
-        ResponseFrame<?> res =  ResponseFrame.ofOKResponse("Success",orderService.updateOrderStatus(orderStatusDto));
+        log.info("관리자 :: 사용자 주문내역 상태 변경 API");
+        ResponseFrame<?> res =  ResponseFrame.ofOKResponse("사용자 주문 상태 변경에 성공했습니다",orderService.updateOrderStatus(orderStatusDto));
+        return new ResponseEntity<>(res, HttpStatus.OK);
+    }
+
+    @ApiOperation(value = "전체 주문 내역 목록 조회")
+    @GetMapping ("/order/list")
+    public ResponseEntity<?> getOrderList(@ApiIgnore @AuthenticationPrincipal UserDto userDto){
+        if(!userDto.getUserRole().equals("ROLE_ADMIN"))
+            throw new GeneralException(ErrorCode.BAD_REQUEST,"관리자 계정이 아닙니다");
+        log.info("관리자 :: 전체 주문 내역 목록 조회 API");
+
+        ResponseFrame<?> res =  ResponseFrame.ofOKResponse("주문 내역 목록을 반환합니다.","");
+        return new ResponseEntity<>(res, HttpStatus.OK);
+    }
+
+    @ApiOperation(value = "전체 주문 건수 조회")
+    @GetMapping ("/order/count")
+    public ResponseEntity<?> getTotalOrderCount(@ApiIgnore @AuthenticationPrincipal UserDto userDto){
+        if(!userDto.getUserRole().equals("ROLE_ADMIN"))
+            throw new GeneralException(ErrorCode.BAD_REQUEST,"관리자 계정이 아닙니다");
+        log.info("관리자 :: 전체 주문 건수 조회 API");
+
+        ResponseFrame<?> res =  ResponseFrame.ofOKResponse("전체 판매 건수를 반환합니다.","");
+        return new ResponseEntity<>(res, HttpStatus.OK);
+    }
+
+    @ApiOperation(value = "일자별 판매 건수 조회")
+    @GetMapping ("/order/count-by-date")
+    public ResponseEntity<?> getOrderCountByDate(@ApiIgnore @AuthenticationPrincipal UserDto userDto){
+        if(!userDto.getUserRole().equals("ROLE_ADMIN"))
+            throw new GeneralException(ErrorCode.BAD_REQUEST,"관리자 계정이 아닙니다");
+        log.info("관리자 :: 일자별 판매 건수 조회 API");
+
+        ResponseFrame<?> res =  ResponseFrame.ofOKResponse("일자별 판매 건수를 반환합니다.","");
+        return new ResponseEntity<>(res, HttpStatus.OK);
+    }
+
+    @ApiOperation(value = "유저 목록 조회")
+    @GetMapping ("/user/list")
+    public ResponseEntity<?> getUserList(@ApiIgnore @AuthenticationPrincipal UserDto userDto){
+        if(!userDto.getUserRole().equals("ROLE_ADMIN"))
+            throw new GeneralException(ErrorCode.BAD_REQUEST,"관리자 계정이 아닙니다");
+        log.info("관리자 :: 유저 목록 조회 API");
+
+        ResponseFrame<?> res =  ResponseFrame.ofOKResponse("유저 목록을 반환합니다.","");
         return new ResponseEntity<>(res, HttpStatus.OK);
     }
 
--- backend/ddokbun/src/main/java/com/harryporter/ddokbun/domain/user/repository/UserRepository.java
+++ backend/ddokbun/src/main/java/com/harryporter/ddokbun/domain/user/repository/UserRepository.java
@@ -1,6 +1,5 @@
 package com.harryporter.ddokbun.domain.user.repository;
 
-import com.harryporter.ddokbun.domain.user.dto.UserDto;
 import com.harryporter.ddokbun.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -9,6 +8,5 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUserEmail(String email);
     Optional<User> findByUserSeq(Long userSeq);
-
     User findByUserNickname(String nickName);
 }
