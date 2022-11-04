27d4a8c1c31bcdca342f7e2b3909958d5c12fd99
이재환 <eejh22@gmail.com>
2022-11-04 17:18:03 +0900

    Resolve S07P31D208-200 "Feature/ be api추가구현"

--- backend/ddokbun/src/main/java/com/harryporter/ddokbun/domain/order/repository/OrderRepository.java
+++ backend/ddokbun/src/main/java/com/harryporter/ddokbun/domain/order/repository/OrderRepository.java
@@ -7,10 +7,15 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order,Long> {
     @Query("SELECT o FROM Order o LEFT JOIN FETCH o.item i LEFT JOIN FETCH i.plant WHERE o.user = :user")
     List<Order> findByUserSeq(@Param("user") User user);
+
+    @Query("SELECT o FROM Order o WHERE o.orderTime >= :start AND o.orderTime <= :end")
+    List<Order> findAllByOrderTime(@Param("start") LocalDate start, @Param("end") LocalDate end);
 }
