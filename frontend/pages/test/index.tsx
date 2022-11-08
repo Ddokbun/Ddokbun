f6381ff1ae533aa41a910c149e301f6ad8bb19f1
신혜원 <hehe2you@gmail.com>
2022-11-08 14:11:44 +0900

    S07P31D208-296 백그라운드 알림 설정

--- frontend/pages/test/index.tsx
+++ frontend/pages/test/index.tsx
@@ -1,22 +1,9 @@
-import { getToken } from "firebase/messaging";
 import { NextPage } from "next";
 import React, { useEffect } from "react";
-import { getTokenFromFB } from "../../apis/firbase";
-import icon from "../../assets/icon/call.svg";
 const test: NextPage = () => {
   const sendMessage = () => {
-    const title = "궁금해 약";
-    const body = "약을 복용할 시간입니다!";
-    const options = { body };
 
-    const notif = new Notification(title, options);
   };
-  useEffect(() => {
-    async function getMessageToken() {
-      await getTokenFromFB();
-    }
-    getMessageToken();
-  }, []);
 
   const btnClickHandler = async () => {
     const result = await Notification.requestPermission();
