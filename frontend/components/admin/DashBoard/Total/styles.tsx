63291879fd23c90c96a8194952cbdaa02ff8a107
김채리 <kcr7812@naver.com>
2022-11-08 17:56:26 +0900

    S07P31D208-246 관리자페이지 대시보드 api 연결

---
+++ frontend/components/admin/DashBoard/Total/styles.tsx
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  margin: 50px 400px;
+  .div-flex {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  .div-wrapper {
+    background-color: #fafafa;
+    width: 330px;
+    margin: 10px;
+    height: 100%;
+    padding: 20px;
+    border-radius: 5px;
+    box-shadow: 0 4px 5px rgba(0, 0, 0, 0.2);
+  }
+  h2 {
+    font-family: ${props => props.theme.font.TitleFont};
+    font-size: 25px;
+    color: grey;
+  }
+  .order-data {
+    font-family: ${props => props.theme.font.TitleFont};
+    font-size: 41px;
+    display: flex;
+    justify-content: center;
+    margin: 10px;
+  }
+`;
