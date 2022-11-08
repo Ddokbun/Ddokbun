50752a6c8b78788e7b27a3c7776f9a9af36c39e6
신혜원 <hehe2you@gmail.com>
2022-11-08 10:03:58 +0900

    S07P31D208-292 firebase token 받기

---
+++ frontend/apis/firebase.ts
@@ -0,0 +1,26 @@
+import firebase from "firebase/app";
+
+export async function getToken() {
+  if (firebase.apps.length) {
+    const messaging = firebase.messaging();
+    const token = await messaging.getToken({
+      vapidKey: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_VAPID_KEY,
+    });
+    return token;
+  } else {
+    firebase.initializeApp({
+      apiKey: "AIzaSyCwdWKZo4h03IqLGmInSPIsDtArvtIJzpA",
+      authDomain: "ddokbun-89ed0.firebaseapp.com",
+      projectId: "ddokbun-89ed0",
+      storageBucket: "ddokbun-89ed0.appspot.com",
+      messagingSenderId: "977818145024",
+      appId: "1:977818145024:web:4b33968439dff5cd1063e5",
+      measurementId: "G-PZ8KF65XZ7",
+    });
+    const messaging = firebase.messaging();
+    const token = await messaging.getToken({
+      vapidKey: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_VAPID_KEY,
+    });
+    return token;
+  }
+}
