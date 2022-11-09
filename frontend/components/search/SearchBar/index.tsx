9fec0e93e2aa25dd1b558070e94f8763f9c2e2e6
kimchaeri <kcr7812@naver.com>
2022-11-09 17:06:18 +0900

    S07P31D208-250 식물검색 중간저장

--- frontend/components/search/SearchBar/index.tsx
+++ frontend/components/search/SearchBar/index.tsx
@@ -1,13 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Wrapper } from "./styles";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
-function SearchBar() {
+import { SearchInput } from "../../../common/Input";
+
+const SearchBar = ({ plants }) => {
+  const [searchInput, setSearchInput] = useState("");
+  const [plantList, setPlantList] = useState(plants);
+
+  useEffect(() => {
+    if (searchInput === "" || searchInput === null) {
+      setPlantList(plants);
+      return;
+    }
+    const newData = plants.filter(plant => {
+      return plant.plantName.includes(searchInput);
+    });
+
+    setPlantList(newData);
+  }, [plants, searchInput]);
   return (
     <Wrapper>
       <div className="title">
         <h2>어떤 식물을 찾고 계신가요?</h2>
       </div>
+      <SearchInput
+        placeholder="찾는 식물 이름을 검색해주세요."
+        disabled={false}
+        setSearchInput={setSearchInput}
+        value={searchInput}
+      ></SearchInput>
+
       <div className="wrap">
         <div className="search">
           <FontAwesomeIcon icon={faMagnifyingGlass} />
@@ -20,6 +43,6 @@ function SearchBar() {
       </div>
     </Wrapper>
   );
-}
+};
 
 export default SearchBar;
