7ba681956b0c43391645da249e52285c674374d0
heogeon0 <heo3793@naver.com>
2022-11-11 16:30:44 +0900

    S07P31D208-304 모바일 네브바 지정

--- frontend/common/Navbar/index.tsx
+++ frontend/common/Navbar/index.tsx
@@ -1,37 +1,104 @@
-import React from "react";
-import { Wrapper } from "./styles";
+import React, { useEffect, useRef, useState } from "react";
+import { Slider, Wrapper } from "./styles";
 
 import Bag from "../../assets/commerce/bag.svg";
-import LeftArrow from "../../assets/commerce/leftArrow.svg";
+import User from "../../assets/commerce/user.svg";
+
 import Link from "next/link";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useRouter } from "next/router";
 
 const Navbar = () => {
+  const ref = useRef<HTMLDivElement>(null);
   const userseq = 1;
+  const [slider, setSlider] = useState(false);
+  const toggleButton = () => {
+    if (ref.current) {
+      ref.current.classList.toggle("open");
+      setSlider(val => !val);
+    }
+    // ref.current.classList.value.toggle("open");
+  };
+  const route = useRouter();
+  useEffect(() => {
+    if (ref.current) {
+      ref.current.classList.toggle("open");
+    }
+    setSlider(false);
+  }, [route]);
+
+  const NabAni = {
+    open: {
+      x: 0,
+      transition: {
+        duration: 0.5,
+      },
+    },
+    closed: {
+      x: "-100%",
+      transition: {
+        duration: 0.5,
+      },
+    },
+  };
 
   return (
-    <Wrapper>
-      <div className="img_wrap">
-        <LeftArrow className="left_arrow nav_svg " />
-      </div>
-      <Link href={"/welcome"}>
-        <div className="logo">Ddokbbun</div>
-      </Link>
-      <div className="img_wrap">
-        <Bag className="bag nav_svg" />
-      </div>
-      <div className="menu">
-        <ul>
-          <Link href={`/manage/${userseq}`}>IoT</Link>
-          <Link href={"/commerce"}>
-            <li>Shopping</li>
-          </Link>
-          <Link href={"/search"}>Search</Link>
-          <Link href={`/mypage/${userseq}`}>
-            <li>MyPage</li>
-          </Link>
-        </ul>
-      </div>
-    </Wrapper>
+    <>
+      <Wrapper>
+        <div className="icon-wrapper">
+          <div ref={ref} id="nav-icon2" onClick={toggleButton}>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+        </div>
+
+        <div className="logo">
+          <Link href={"/welcome"}>Ddokbbun</Link>
+        </div>
+
+        <div className="img_wrap">
+          <Bag />
+          <User viewBox="0 0 512 512" />
+        </div>
+      </Wrapper>
+
+      <Slider variants={NabAni} animate={slider ? "open" : "closed"}>
+        <div className="menu">
+          <ul>
+            <Link href={`/manage/${userseq}`}>
+              <li>
+                <div className="title">IoT</div>
+              </li>
+            </Link>
+            <Link href={"/commerce"}>
+              <li>
+                <div className="title">Shopping</div>
+                <div className="drop-down open">
+                  <li>초보집사</li>
+                  <li>집꾸미기</li>
+                  <li>공기정화</li>
+                  <li>애완동물</li>
+                </div>
+              </li>
+            </Link>
+            <Link href={"/search"}>
+              <li>
+                <div className="title">Search</div>
+              </li>
+            </Link>
+            <Link href={`/mypage/${userseq}`}>
+              <li>
+                <div className="title">MyPage</div>
+              </li>
+            </Link>
+          </ul>
+        </div>
+      </Slider>
+    </>
   );
 };
 
--- frontend/common/Navbar/styles.tsx
+++ frontend/common/Navbar/styles.tsx
@@ -1,27 +1,51 @@
+import { motion } from "framer-motion";
 import styled from "styled-components";
 
 export const Wrapper = styled.div`
-  position: sticky;
+  background-color: #fafafa;
+  position: fixed;
   z-index: 1;
   top: 0;
   width: 100%;
-  height: 50px;
-  background-color: ${props => props.theme.color.black};
+  height: 80px;
   display: flex;
   justify-content: space-between;
   align-items: center;
-  padding: 0px 10px;
+  padding: 0px 30px;
+
   font-family: ${props => props.theme.font.EnglishFont};
   font-size: 18px;
   .logo {
-    order: -1;
-    color: ${props => props.theme.color.ivory};
-    font-size: 25px;
+    position: absolute;
+    left: 50%;
+    transform: translate(-50%, 0%);
+    color: ${props => props.theme.color.darkGreen};
+    font-size: 40px;
   }
+  .img_wrap {
+    width: 100px;
+    display: flex;
+    justify-content: space-evenly;
 
-  .nav_svg {
-    display: none;
-    cursor: pointer;
+    svg {
+      width: 40px;
+      height: 40px;
+      cursor: pointer;
+      path {
+        fill: transparent;
+        stroke: ${props => props.theme.color.darkGreen};
+        stroke-width: 3;
+      }
+    }
+
+    .user_svg__user {
+      width: 25px;
+      path {
+        fill: ${props => props.theme.color.darkGreen};
+        stroke: ${props => props.theme.color.darkGreen};
+        stroke-width: 20;
+      }
+    }
   }
 
   .menu {
@@ -29,17 +53,115 @@ export const Wrapper = styled.div`
       display: flex;
       flex-direction: row;
       align-items: center;
-      color: ${props => props.theme.color.ivory};
+      font-size: 24px;
+      color: ${props => props.theme.color.darkGreen};
       li {
         margin: 20px;
         cursor: pointer;
       }
     }
   }
+  .icon-wrapper {
+    display: flex;
+    align-items: center;
+  }
+  #nav-icon2 {
+    cursor: pointer;
+    width: 30px;
+    height: 25px;
+    position: relative;
+  }
+
+  #nav-icon2 span {
+    display: block;
+    position: absolute;
+    height: 5px;
+    width: 50%;
+    background: ${props => props.theme.color.darkGreen};
+    opacity: 1;
+    -webkit-transform: rotate(0deg);
+    -moz-transform: rotate(0deg);
+    -o-transform: rotate(0deg);
+    transform: rotate(0deg);
+    -webkit-transition: 0.25s ease-in-out;
+    -moz-transition: 0.25s ease-in-out;
+    -o-transition: 0.25s ease-in-out;
+    transition: 0.25s ease-in-out;
+  }
+
+  #nav-icon2 span:nth-child(even) {
+    left: 50%;
+    border-radius: 0 9px 9px 0;
+  }
+
+  #nav-icon2 span:nth-child(odd) {
+    left: 0px;
+    border-radius: 9px 0 0 9px;
+  }
+
+  #nav-icon2 span:nth-child(1),
+  #nav-icon2 span:nth-child(2) {
+    top: 0px;
+  }
+
+  #nav-icon2 span:nth-child(3),
+  #nav-icon2 span:nth-child(4) {
+    top: 10px;
+  }
+
+  #nav-icon2 span:nth-child(5),
+  #nav-icon2 span:nth-child(6) {
+    top: 20px;
+  }
+
+  #nav-icon2.open span:nth-child(1),
+  #nav-icon2.open span:nth-child(6) {
+    -webkit-transform: rotate(45deg);
+    -moz-transform: rotate(45deg);
+    -o-transform: rotate(45deg);
+    transform: rotate(45deg);
+  }
+
+  #nav-icon2.open span:nth-child(2),
+  #nav-icon2.open span:nth-child(5) {
+    -webkit-transform: rotate(-45deg);
+    -moz-transform: rotate(-45deg);
+    -o-transform: rotate(-45deg);
+    transform: rotate(-45deg);
+  }
+
+  #nav-icon2.open span:nth-child(1) {
+    left: 5px;
+    top: 7px;
+  }
+
+  #nav-icon2.open span:nth-child(2) {
+    left: calc(50% - 5px);
+    top: 7px;
+  }
+
+  #nav-icon2.open span:nth-child(3) {
+    left: -50%;
+    opacity: 0;
+  }
+
+  #nav-icon2.open span:nth-child(4) {
+    left: 100%;
+    opacity: 0;
+  }
+
+  #nav-icon2.open span:nth-child(5) {
+    left: 5px;
+    top: 12px;
+  }
+
+  #nav-icon2.open span:nth-child(6) {
+    left: calc(50% - 5px);
+    top: 12px;
+  }
 
   @media screen and (${props => props.theme.mobile}) {
-    position: sticky;
-    padding: 0px 0px;
+    padding: 0px 10px;
 
     .menu {
       display: none;
@@ -55,22 +177,91 @@ export const Wrapper = styled.div`
 
     .img_wrap {
       height: 100%;
-      width: 50px;
+      width: 60px;
+      gap: 10px;
       display: flex;
       align-items: center;
       justify-content: center;
     }
-
-    .bag {
-      width: 30px;
-
-      fill: ${props => props.theme.color.ivory};
-      stroke: ${props => props.theme.color.ivory};
+    #nav-icon2 {
+      cursor: pointer;
+      width: 25px;
+      height: 20px;
+      position: relative;
     }
 
-    .left_arrow {
-      width: 30px;
-      fill: ${props => props.theme.color.ivory};
+    #nav-icon2 span {
+      height: 3px;
+      width: 50%;
+    }
+
+    #nav-icon2 span:nth-child(3),
+    #nav-icon2 span:nth-child(4) {
+      top: 8px;
+    }
+
+    #nav-icon2 span:nth-child(5),
+    #nav-icon2 span:nth-child(6) {
+      top: 16px;
+    }
+
+    #nav-icon2.open span:nth-child(1) {
+      left: 5px;
+      top: 7px;
+    }
+
+    #nav-icon2.open span:nth-child(5) {
+      left: 5px;
+      top: 10px;
+    }
+
+    #nav-icon2.open span:nth-child(6) {
+      left: calc(50% - 5px);
+      top: 10px;
+    }
+  }
+`;
+
+export const Slider = styled(motion.div)`
+  padding: 20px;
+  top: 80px;
+  opacity: 1;
+  position: fixed;
+  background-color: #fafafa;
+  z-index: 1;
+  width: 100%;
+  height: calc(100vh - 80px);
+
+  .menu {
+    ul {
+      li .title {
+        margin: 5px 0px;
+        color: ${props => props.theme.color.darkGreen};
+        padding-bottom: 2px;
+        border-bottom: 1px solid ${props => props.theme.color.darkGreen};
+        font-size: 24px;
+        font-family: ${props => props.theme.font.TextFont2};
+
+        :hover {
+          cursor: pointer;
+        }
+      }
+    }
+
+    .drop-down {
+      display: none;
+      margin: 20px 0px 10px 40px;
+      padding-bottom: 10px;
+      /* border-bottom: 1px solid ${props => props.theme.color.darkGreen}; */
+      li {
+        margin: 5px 0px;
+        font-size: 18px;
+        color: ${props => props.theme.color.darkGreen};
+        font-family: ${props => props.theme.font.TextFont2};
+      }
+    }
+    .open {
+      display: block !important;
     }
   }
 `;
