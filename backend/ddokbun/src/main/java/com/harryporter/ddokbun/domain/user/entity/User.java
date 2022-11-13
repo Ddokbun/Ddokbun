52feb5088c2644eca67f31cdda13129251eed332
손광진 <start_so@naver.com>
2022-11-14 08:58:58 +0900

    S07P31D208-275 자동 물주기 로직 조정

--- backend/ddokbun/src/main/java/com/harryporter/ddokbun/domain/user/entity/User.java
+++ backend/ddokbun/src/main/java/com/harryporter/ddokbun/domain/user/entity/User.java
@@ -21,7 +21,7 @@ import java.util.List;
 public class User {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name="user_seq",columnDefinition = "INTEGER UNSIGNED")
     private Long userSeq;
 
