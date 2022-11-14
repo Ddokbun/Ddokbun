4a9940b059280aebe8b602671de308a3dea8cddd
신혜원 <hehe2you@gmail.com>
2022-11-14 22:37:12 +0900

    S07P31D208-334 주문내역 조회 디자인

--- frontend/components/mypage/OrderDetailCard/index.tsx
+++ frontend/components/mypage/OrderDetailCard/index.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { FC } from "react";
 import { ItemListType } from "../DeliveryCard/Index";
+import { Wrapper } from "./styles";
 
 interface Props {
   data: ItemListType;
@@ -9,13 +10,15 @@ interface Props {
 
 const OrderDetailCard: FC<Props> = ({ data }) => {
   return (
-    <div>
+    <Wrapper>
       <Image src={data.itemPicture} width="100%" height={"100%"} />
-      {data.itemName}
-      {data.itemEnName}
-      {data.itemPrice}
-      <Link href={`/commerce/product/${data.itemSeq}`}>상세보기</Link>
-    </div>
+      <div className="info">
+        <p className="kr-name">{data.itemName}</p>
+        <p className="en-name">{data.itemEnName}</p>
+        <p>{data.itemPrice}원</p>
+      </div>
+      <Link className="link" href={`/commerce/product/${data.itemSeq}`}>식물 상세 보기</Link>
+    </Wrapper>
   );
 };
 
