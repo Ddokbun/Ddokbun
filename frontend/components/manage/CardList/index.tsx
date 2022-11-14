58d77f9a5c22f51dc90cd2b15f70f76f7fc6d0b1
신혜원 <hehe2you@gmail.com>
2022-11-15 00:00:13 +0900

    S07P31D208-329 관리 홈 스와이퍼로 변경

--- frontend/components/manage/CardList/index.tsx
+++ frontend/components/manage/CardList/index.tsx
@@ -1,8 +1,15 @@
 import { motion } from "framer-motion";
 import { FC } from "react";
+import { Navigation, Pagination } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
 import { PlantListType } from "../../../pages/manage/[userseq]";
 import CardItem from "../CardItem";
 import { Wrapper } from "./styles";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
 
 interface Props {
   plantsList: PlantListType[];
@@ -11,17 +18,23 @@ interface Props {
 const CardList: FC<Props> = ({ plantsList }) => {
   return (
     <Wrapper>
-      {plantsList?.map(plant => {
-        return (
-          <motion.li key={plant.potSerial}>
-            <CardItem
-              plantSeq={plant.plantSeq}
-              potSerial={plant.potSerial}
-              plantNickname={plant.plantNickname}
-            />
-          </motion.li>
-        );
-      })}
+      <Swiper
+        navigation={true}
+        slidesPerView={1}
+        modules={[Navigation, Pagination]}
+      >
+        {plantsList?.map(plant => {
+          return (
+            <SwiperSlide key={plant.potSerial}>
+              <CardItem
+                plantSeq={plant.plantSeq}
+                potSerial={plant.potSerial}
+                plantNickname={plant.plantNickname}
+              />
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
     </Wrapper>
   );
 };
