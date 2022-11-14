4ec77546c0090f4a0e6829f39f68937f25d8bb24
신혜원 <hehe2you@gmail.com>
2022-11-14 21:28:51 +0900

    S07P31D208-334 주문내역 조회 디자인

--- frontend/components/mypage/DeliveryCard/styles.tsx
+++ frontend/components/mypage/DeliveryCard/styles.tsx
@@ -1,20 +1,38 @@
 import styled from "styled-components";
 
-export const Wrapper = styled.div`
-  display: grid;
-  grid-template-columns: repeat(2, 1fr);
-  width: 350px;
-
-  p {
-    font-family: ${props => props.theme.font.TextFont2};
-  }
-  .button-container {
-    font-family: ${props => props.theme.font.TextFont2};
+export const Wrapper = styled.tr`
+  height: 130px;
+  border-bottom: 1px solid #ccc;
+  .image-container {
     display: flex;
+    align-items: center;
+    margin: 16px;
     justify-content: space-around;
+  }
+  :nth-child(1) {
+  }
+  :nth-child(2) {
+    margin-left: 16px;
   }
 
   button {
-    margin: 4%;
+    font-size: 12px;
+  }
+
+  .info {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+  }
+  .info > p {
+    font-family: ${props => props.theme.font.TextFont2};
+  }
+  @media screen and (${props => props.theme.mobile}) {
+    p {
+      font-size: 12px;
+    }
+    button {
+      font-size: 8px;
+    }
   }
 `;
