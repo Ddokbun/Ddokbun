c10b3677abd6d166ff20dded9616517c81ad9dd4
신혜원 <hehe2you@gmail.com>
2022-11-14 17:29:26 +0900

    S07P31D208-294 현재 상태 로직 구현

--- frontend/pages/search/camera/index.tsx
+++ frontend/pages/search/camera/index.tsx
@@ -1,73 +1,31 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useCallback, useEffect, useRef, useState } from "react";
+import Webcam from "react-webcam";
 
-interface RTCVideoProps {
-  mediaStream: MediaStream | undefined;
-}
-
-const Camera: React.FC<{ photo: any; video: any }> = () => {
-  const FACING_MODE_USER = "user";
-  const FACING_MODE_ENVIRONMENT = "environment";
-  const [facingMode, setFacingMode] = useState(FACING_MODE_USER);
-
-  let videoRef = useRef<HTMLVideoElement | any>(null);
-  let photoRef = useRef<HTMLVideoElement | any>(null);
-
-  const videoConstraints = {
-    facingMode: FACING_MODE_USER,
-  };
-
-  const getUserCamera = () => {
-    const devices = navigator.mediaDevices.enumerateDevices();
-    console.log("사용가능한 장치", devices);
-    navigator.mediaDevices
-      .getUserMedia({
-        video: {
-          facingMode: "environment",
-        },
-      })
-      .then(stream => {
-        let video = videoRef.current;
-        video.srcObject = stream;
-        video.play();
-      })
-      .catch(error => {
-        alert("장치에 접근하지 못하고 있습니다");
-        console.log("절대안된다잉", error);
-      });
-  };
-
-  useEffect(() => {
-    getUserCamera();
-  }, [videoRef]);
-
-  const takePicture = () => {
-    let width = 500;
-    let height = 200;
-    let photo = photoRef.current;
-    let video = videoRef.current;
-
-    photo.width = width;
-    photo.height = height;
-
-    let ctx = photo.getContext("2d");
-    ctx.drawImage(video, 0, 0, photo.width, photo.height);
-  };
-
-  const clearPicture = () => {
-    let photo = photoRef.current;
-
-    let ctx = photo.getContext("2d");
-    ctx.clearRect(0, 0, photo.width, photo.height);
-  };
+const Camera = () => {
+  const [image, setImage] = useState("");
+  const webcamRef = useRef(null);
+  //   const capture = useCallback(() => {
+  //     const imageSrc = webcamRef.current.getScreenshot();
+  //     setImage(imageSrc);
+  //   }, [webcamRef]);
 
   return (
-    <div className="container">
-      <input type="file" />
-      <h1>사진을 촬영해주세요</h1>
-      <video className="container" ref={videoRef}></video>
-      <button onClick={takePicture}> Take Picture</button>
-      <canvas className="container" ref={photoRef}></canvas>
-      <button onClick={clearPicture}> Clear Picture</button>
+    <div>
+      <Webcam
+        audio={false}
+        height={720}
+        ref={webcamRef}
+        screenshotFormat="image/jpeg"
+        videoConstraints={{
+          facingMode: "environment",
+        }}
+        onUserMediaError={() =>
+          window.alert("카메라 기기에 접근할 수 없습니다.")
+        }
+      />
+      {/* <button onClick={capture}>캡쳐</button> */}
+      <img src={image} />
+      <button>전송하기</button>
     </div>
   );
 };
