d3b8910599a880cea2fc7b393733c597892fddcb
kimchaeri <kcr7812@naver.com>
2022-11-15 09:44:09 +0900

    S07P31D208-301 스마트화분 배너

--- frontend/components/commerce/main/PotBanner/index.tsx
+++ frontend/components/commerce/main/PotBanner/index.tsx
@@ -14,7 +14,18 @@ const PotBanner = () => {
           objectFit="contain"
         />
       </div>
-      <div>가나다라 마바사아</div>
+      <div className="text-wrap">
+        <div className="text">
+          <h2>Ddokbun's Smart Plant Pot</h2>
+          <h3>
+            똑똑한 화분, 똑분에서 직접 제작하고 판매하는 스마트 화분이에요.
+          </h3>
+          <h3>
+            키우는 식물의 온도, 습도, 토양습도, 물 주기까지 한 번에 확인하고,
+          </h3>
+          <h3>화분을 키울 곳의 생활 환경에 맞게 관리를 도와드릴게요.</h3>
+        </div>
+      </div>
     </Wrapper>
   );
 };
--- frontend/components/commerce/main/PotBanner/styles.tsx
+++ frontend/components/commerce/main/PotBanner/styles.tsx
@@ -8,9 +8,53 @@ export const Wrapper = styled.div`
     position: relative;
     height: 500px;
     width: 500px;
+    overflow: hidden;
+  }
+  .text-wrap {
+    display: flex;
+    align-items: center;
+    margin-left: 70px;
+    h2 {
+      font-size: 35px;
+      margin-bottom: 25px;
+    }
+    h3 {
+      font-family: ${props => props.theme.font.TextFont2};
+      font-size: 17px;
+    }
+  }
+
+  @media screen and (max-width: 1024px) {
+    .text-wrap {
+      margin-left: 40px;
+      h2 {
+        font-size: 30px;
+        margin-bottom: 25px;
+      }
+      h3 {
+        font-size: 14px;
+      }
+    }
   }
 
   @media screen and (max-width: 600px) {
-    height: 500px;
+    display: block;
+    .img-wrap {
+      margin-bottom: 20px;
+    }
+    .text-wrap {
+      display: block;
+      margin: 20px auto;
+      text-align: center;
+      .text {
+        padding-bottom: 20px;
+      }
+
+      h2 {
+        font-size: 28px;
+      }
+      h3 {
+      }
+    }
   }
 `;
