404063fd2e84edfd2a2b7def3378dc0f5b446c9a
이동욱 <socialable@naver.com>
2022-11-17 16:46:36 +0900

    S07P31D208-355 Feature/ 파이프라인구성

--- backend/ddokbun/src/main/java/com/harryporter/ddokbun/domain/plant/repository/PotRepository.java
+++ backend/ddokbun/src/main/java/com/harryporter/ddokbun/domain/plant/repository/PotRepository.java
@@ -1,5 +1,6 @@
 package com.harryporter.ddokbun.domain.plant.repository;
 
+import com.harryporter.ddokbun.domain.plant.entity.Plant;
 import com.harryporter.ddokbun.domain.plant.entity.Pot;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -8,4 +9,5 @@ import java.util.Optional;
 public interface PotRepository extends JpaRepository<Pot, Long> {
     Optional<Pot> findByPotSerial(String potSerial);
     Boolean existsByPotSerial(String potSerial);
+
 }
