171395a3974e5dc33ea90fa8030b8ed55a8a0817
손광진 <start_so@naver.com>
2022-11-17 16:27:24 +0900

    S07P31D208-330 "Feature/ 백엔드 서버 테스트"

--- iot/raspberry/ddokbunProcess.sh
+++ iot/raspberry/ddokbunProcess.sh
@@ -1,4 +1,11 @@
 pwd
 whoami
 sleep 5
-python3 /home/ddokbun/iot/SerialToArduino.py
+VAR1=1
+VAR2=1
+while true
+do
+	echo "system start : $VAR1 count"
+	python3 /home/ddokbun/iot/SerialToArduino.py
+	VAR1=$(($VAR1 + $VAR2))
+done
