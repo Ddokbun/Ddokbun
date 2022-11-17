fcfa270a3515df2deb95c2aaa6888df99b8ca1f9
kimchaeri <kcr7812@naver.com>
2022-11-17 17:51:57 +0900

    S07P31D208-318 camera 폴더 수정

--- frontend/components/search/SearchBar/index.tsx
+++ frontend/components/search/SearchBar/index.tsx
@@ -1,15 +1,54 @@
 import React, { useEffect, useState } from "react";
-import { Wrapper } from "./styles";
+import { ModalWapper, Wrapper } from "./styles";
 import { SearchInput } from "../../../common/Input";
 import { PlantListArray } from "../../../types/search/searchbar.interface";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Modal from "react-modal";
 import { faCamera, faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 import Link from "next/link";
+import { SearchBtn } from "../../../common/Button";
+import { Theme } from "../../../styles/theme";
+import { fetchItemSeq, postPicture } from "../../../apis/search";
+import router from "next/router";
 
 const SearchBar: React.FC<{ plants: PlantListArray }> = ({ plants }) => {
   const [searchInput, setSearchInput] = useState("");
   const [plantList, setPlantList] = useState(plants);
+  const [modalIsOpen, setIsOpen] = useState(false);
 
+  const setFile = async (event: any) => {
+    const data = postPicture(event.target.files[0]);
+    const res = fetchItemSeq(await data);
+    const plantSeq = await res;
+    if (plantSeq === undefined) {
+      alert("해당하는 값을 찾지 못했습니다. 다시 검색을 시도해주세요.");
+      window.location.replace("/search/camera");
+    } else {
+      router.push(`/commerce/product/${plantSeq}`);
+    }
+    router.push(`/commerce/product/${plantSeq}`);
+  };
+  function openModal() {
+    setIsOpen(true);
+    console.log("모달열림");
+  }
+  function afterOpenModal() {
+    console.log("모달 변경");
+  }
+  function closeModal() {
+    setIsOpen(false);
+  }
+  const customStyles = {
+    content: {
+      top: "50%",
+      left: "50%",
+      right: "auto",
+      bottom: "auto",
+      marginRight: "-50%",
+      transform: "translate(-50%, -50%)",
+      borderRadius: "10px",
+    },
+  };
   useEffect(() => {
     if (searchInput === "" || searchInput === null) {
       setPlantList(plants);
@@ -34,12 +73,35 @@ const SearchBar: React.FC<{ plants: PlantListArray }> = ({ plants }) => {
           value={searchInput}
           path={"search"}
         ></SearchInput>
-        <Link href={"search/ai-search"}>
+
+        <button onClick={openModal}>
           <div className="camera">
             <FontAwesomeIcon icon={faCamera} size="2x"></FontAwesomeIcon>
             <h3>사진으로 검색하기</h3>
           </div>
-        </Link>
+        </button>
+        <Modal
+          isOpen={modalIsOpen}
+          style={customStyles}
+          onRequestClose={closeModal}
+        >
+          <ModalWapper>
+            <h1>사진으로 식물을 검색해보세요!</h1>
+            <div className="button-wrap">
+              <div className="file-select">
+                <label htmlFor="camera-file">갤러리</label>
+                <input
+                  type={"file"}
+                  id="camera-file"
+                  onChange={setFile.bind(this)}
+                ></input>
+              </div>
+              <SearchBtn color={Theme.color.ivory} path={`search/camera`}>
+                카메라 촬영
+              </SearchBtn>
+            </div>
+          </ModalWapper>
+        </Modal>
       </div>
       <div></div>
     </Wrapper>
--- frontend/components/search/SearchBar/styles.tsx
+++ frontend/components/search/SearchBar/styles.tsx
@@ -62,3 +62,62 @@ export const Wrapper = styled.div`
     }
   }
 `;
+
+export const ModalWapper = styled.div`
+  padding: 0 30px;
+  background-color: #fafafa;
+  margin: 5% auto;
+  max-width: 1240px;
+  width: 100%;
+  text-align: center;
+  h1 {
+    font-family: ${props => props.theme.font.TitleFont};
+    font-size: 40px;
+    margin-bottom: 50px;
+  }
+  .button-wrap {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  .file-select {
+    background-color: ${props => props.theme.color.brown};
+    border-radius: 15px;
+    padding: 3% 0 3%;
+    margin: 1%;
+    width: 150px;
+    height: 150px;
+    box-shadow: 0 4px 5px rgba(0, 0, 0, 0.3);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    cursor: pointer;
+    label {
+      font-size: 20px;
+      font-family: ${props => props.theme.font.TitleFont};
+    }
+  }
+
+  .file-select input {
+    width: 1px;
+    height: 1px;
+    padding: 0;
+
+    overflow: hidden;
+    clip: rect(0, 0, 0, 0);
+    border: 0;
+  }
+
+  @media screen and (max-width: 600px) {
+    .file-select {
+      width: 90px;
+      height: 90px;
+      border-radius: 10px;
+      margin: 1%;
+      padding: 2% 0 2%;
+    }
+    label {
+      font-size: 15px;
+    }
+  }
+`;
